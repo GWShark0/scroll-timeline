d0ea0bc299b0899edb107661eef0ccd7d95690d0
Greg Douglas <greg@storyblocks.com>
2022-04-25 00:23:02 -0400

    refs

--- src/components/Playhead.js
+++ src/components/Playhead.js
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useRef } from 'react';
 import { DraggableCore } from 'react-draggable';
 
 import { ReactComponent as PlayheadHandle } from '../assets/cursor.svg';
@@ -6,14 +6,20 @@ import { ReactComponent as PlayheadHandle } from '../assets/cursor.svg';
 import './Playhead.css';
 
 function Playhead(props) {
+  const nodeRef = useRef(null);
   const { onDragStart, onDrag, onDragEnd, playheadX, scrollX } = props;
 
   const x = playheadX - scrollX;
   const style = { transform: `translate3d(${x}px, 0, 0)` };
 
   return (
-    <DraggableCore onStart={onDragStart} onDrag={onDrag} onStop={onDragEnd}>
-      <div className="playhead" style={style}>
+    <DraggableCore
+      onStart={onDragStart}
+      onDrag={onDrag}
+      onStop={onDragEnd}
+      nodeRef={nodeRef}
+    >
+      <div className="playhead" style={style} ref={nodeRef}>
         <PlayheadHandle className="playhead-handle" />
       </div>
     </DraggableCore>
